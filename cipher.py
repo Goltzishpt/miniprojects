fbe18571f586498d1ceb338e566667fc86b4a0b4
Goltz <95189224+Goltzishpt@users.noreply.github.com>
2022-01-30 00:04:48 +0200

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,6 +1,9 @@
-shift = int(input())
-cipher = input()
-char1 = 0
-for c in range(len(cipher)):
-    char1 = ord(cipher[c])
-    print(chr(char1 - shift), end = '')
+# ЦИФР ЦЕЗАРЯ со сдвигом, только для маленьких буека
+
+
+shift = int(input()) # цифра сдвига
+cipher = input() # шифр
+char1 = 0 # значение буквы по юникоду
+for c in range(len(cipher)): # цикл по ренжу
+    char1 = ord(cipher[c]) # перевод в цифру юникода
+    print(chr(char1 - shift), end = '') # принт буквы через цифру
